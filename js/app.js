7de6f19be13346d7a934667390c88d223d95c494
sajid4807 <mdsajidhossen4807@gmail.com>
2025-08-27 17:27:32 +0600

    added card section js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,155 @@
+function getInnerText (id){
+    let heart = parseInt(document.getElementById(id).innerText)
+    return heart;
+}
+
 function heartValueIncrease (){
-    let heartValue =parseInt(document.getElementById(`heart-value`).innerText)
+    let heartValue = getInnerText(`heart-value`)
     heartValue += 1;
     document.getElementById(`heart-value`).innerText = heartValue
-
 }
-// document.getElementById(`heart`).addEventListener(`click`,function(){
-//     console.log(`hello`)
-//     let heartValue =parseInt(document.getElementById(`heart-value`).innerText)
-//     heartValue += 1;
-//     document.getElementById(`heart-value`).innerText = heartValue
-// })
 
 
+
+function setInnerText (value) {
+    const availableStar = document.getElementById(`star`)
+    availableStar.innerText = value
+}
+
+// card 1
+document.getElementById(`call1`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling National Emergency Number 999`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
+
+// card 2
+document.getElementById(`call2`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling Police Helpline Number 999`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
+
+// card 3
+document.getElementById(`call3`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling Fire Service Number 999`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
+
+// card 4
+document.getElementById(`call4`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling Ambulance Service Number 1994-999999`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
+
+// card 5
+document.getElementById(`call5`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling Women & Child Helpline Number 109`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
+
+// card 6
+document.getElementById(`call6`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling Anti-Corruption Helpline Number 106`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
+
+// card 7
+document.getElementById(`call7`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling Electricity Helpline Number 16216`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
+
+// card 8
+document.getElementById(`call8`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling Brac Helpline Number 16445`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
+
+// card 9
+document.getElementById(`call9`).addEventListener(`click`, function(event){
+    event.preventDefault()
+    let starIcon = getInnerText(`star`)
+    if(starIcon > 0){
+    alert(`Calling Bangladesh Railway Helpline  Number 163`)
+    }
+    if(starIcon <=0){
+        alert(`Insufficient Coin To Make A Call. At Least 20 Coins Are Required`)
+        return;
+    }
+    newAvailableStar = starIcon - 20
+    setInnerText(newAvailableStar)
+})
 
 // heart increase
 document.getElementById(`heart1`).addEventListener(`click`,heartValueIncrease)
